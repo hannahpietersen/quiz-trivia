0bbf05552316cd692340739cf76b11a85a66b8ef
someid <hannahpietersen@gmail.com>
2023-04-13 20:41:27 +0530

    components for app structure created

---
+++ trivia/src/components/Answer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Answer() {
+  return <div>
+    
+  </div>;
+}
+
+export default Answer;
---
+++ trivia/src/components/Question.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Answer from "./Answer";
+
+function Question() {
+  return <div></div>;
+}
+
+export default Question;
