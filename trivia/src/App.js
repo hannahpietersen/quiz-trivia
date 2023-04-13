2bc617ec5c9bba3d093ef523aa3260d313424a31
someid <hannahpietersen@gmail.com>
2023-04-13 20:43:01 +0530

    small changes made

--- trivia/src/App.js
+++ trivia/src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Introduction from "./components/Introduction";
+import Question from "./components/Question";
+import { nanoid } from "nanoid";
+import ReactConfetti from "react-confetti";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Introduction />
+    </div>
+  );
 }
 
 export default App;
